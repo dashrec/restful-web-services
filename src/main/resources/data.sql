872b0f3c9daefdf79339fef895152bf4590cd209
dashrec <95615608+dashrec@users.noreply.github.com>
2023-04-19 20:17:24 +0200

    Last Commit

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into todo(id,description,done,target_Date,username)values(102,'insert from datasql', false, CURRENT_DATE(), 'dash');  
-insert into todo(id,description,done,target_Date,username)values(103,'aws derver', false, CURRENT_DATE(), 'dash');  
-insert into todo(id,description,done,target_Date,username)values(104,'test description', false, CURRENT_DATE(), 'dash');  
-
